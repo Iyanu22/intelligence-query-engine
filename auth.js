1d65acabfa44a38819830529a153eeeb12dbd604
Iyanu22 <olajideiyanu22@gmail.com>
2026-04-29 12:47:39 +0100

    fix(auth): extend refresh token to 7 days

--- auth.js
+++ auth.js
@@ -16,8 +16,8 @@ function generateAccessToken(user) {
 // Generate refresh token (5 mins)
 async function generateRefreshToken(userId) {
   const token = uuidv4();
-  const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes
-
+  // const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes
+const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days
   await pool.query(
     `INSERT INTO refresh_tokens (id, user_id, token, expires_at) VALUES ($1, $2, $3, $4)`,
     [uuidv4(), userId, token, expiresAt]
